09cac157f77f4329fa04ec00a83e2d1de5e5c2b3
DanielVieiraSS <danielvieira2828@gmail.com>
2024-02-27 18:38:19 -0300

    .

--- lib/categories.dart
+++ lib/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class Categories extends StatelessWidget {
   const Categories({super.key});
@@ -10,13 +12,17 @@ class Categories extends StatelessWidget {
         title: const Text("Pick your category"),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: [],
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
+        ],
       ),
     );
   }
