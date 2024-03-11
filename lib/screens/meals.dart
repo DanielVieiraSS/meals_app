6ccd8e5a2021f3e70594c9b754a1bc18173a5abd
DanielVieiraSS <danielvieira2828@gmail.com>
2024-03-11 13:42:36 -0300

    .

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -8,19 +8,16 @@ class MealsScreen extends StatelessWidget {
     super.key,
     this.title,
     required this.meals,
-    required this.onToggleFavorite,
   });
 
   final String? title;
   final List<Meal> meals;
-  final void Function(Meal meal) onToggleFavorite;
 
   void selectMeal(BuildContext context, Meal meal) {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (ctx) =>
-            MealDetailsScreen(onToggleFavorite: onToggleFavorite, meal: meal),
+        builder: (ctx) => MealDetailsScreen(meal: meal),
       ),
     );
   }
